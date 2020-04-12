53ab1e2536c5f95a450b9755587189e090fb9e6f
Sihuta <63560452+Sihuta@users.noreply.github.com>
2020-04-12 23:12:21 +0300

    Add files via upload

---
+++ Field.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PacmanGame
+{
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Ch { get; set; }
+
+        public Point(int x, int y, char ch)
+        {
+            X = x;
+            Y = y;
+            Ch = ch;
+        }
+
+        public void Draw()
+        {
+            DrawPoint(Ch);
+        }
+        public void Clear()
+        {
+            DrawPoint(' ');
+        }
+        private void DrawPoint(char _ch)
+        {
+            Console.SetCursorPosition(X, Y);
+
+            if (_ch == 'o')
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            if (_ch == '#')
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+            }
+            if (_ch == 'A')
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if (_ch == '@')
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            if (_ch == 'V')
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            if (_ch == '.')
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+
+
+    public class Field
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public Point[,] Matrix;
+
+        public Field(int rows, int columns)
+        {
+            if (rows >= 5 && columns >= 5)
+            {
+                Rows = rows;
+                Columns = columns;
+            }
+            else
+                throw new ArgumentException("Указанный размер поля очень мал");
+        }
+
+        public Point[,] CreateMatrix()
+        {
+            Point[,] matrix = new Point[Rows, Columns];
+            Point point;
+
+            for (int i = 0; i < Rows; ++i)
+            {
+                for (int j = 0; j < Columns; ++j)
+                {
+                    if (i == 0 || i == Rows - 1)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if (j == 0 || j == Columns - 1)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if (j == 2 && i >= 2 && i <= Rows - 1)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+                    else if (j == Columns - 3 && i >= 2 && i <= Rows - 1)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if (j == 4 && i >= 2 && i <= Rows - 1 && i != Rows / 2)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if (j == Columns - 5 && i >= 2 && i <= Rows - 1 && i != Rows / 2)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if ((i == 2 || i == Rows - 3) && j > 4 && j < Columns - 4 && j != Columns / 2)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if (i == 4 && j > 6 && j < Columns - 6)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if ((j == 6 || j == Columns - 7) && i > 4 && i < Rows - 4)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if ((i == 6 || i == Rows - 5) && j >= 8 && j <= Columns - 9)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if ((j == Columns / 2 - 1 || j == Columns / 2 + 1) && i >= 7 && i < Rows - 6)
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else if (i == 8 && ((j >= 8 && j < Columns / 2 - 2) || (j > Columns / 2 + 2 && j < Columns - 8)))
+                    {
+                        point = new Point(j, i, '#');
+                        matrix[i, j] = point;
+                    }
+
+                    else
+                    {
+                        matrix[i, j] = Random(i, j);
+                    }
+                }
+            }
+            var pacman = new Point(Columns / 2, Rows / 2, ' ');
+            matrix[Rows / 2, Columns / 2] = pacman;
+
+            return matrix;
+        }
+
+        public void PrintField(Point[,] field)
+        {
+            for (int i = 0; i < Rows; ++i)
+            {
+                for (int j = 0; j < Columns; ++j)
+                {
+                    field[i, j].Draw();
+                    Console.Write(field[i, j].Ch);
+                }
+            }
+        }
+
+        public void GhostNeutrolise()
+        {
+            GhostNeutrolise(Matrix);
+        }
+        public void GhostNeutrolise(Point[,] field)
+        {
+            for (int i = 1; i < Rows - 1; ++i)
+            {
+                for (int j = 1; j < Columns - 1; ++j)
+                {
+                    if (field[i, j].Ch == 'A')
+                    {
+                        field[i, j].Ch = 'V';
+                        field[i, j].Draw();
+                        Console.Write(field[i, j].Ch);
+                    }
+                }
+            }
+        }
+        public void GhostActive()
+        {
+            GhostActive(Matrix);
+        }
+        public void GhostActive(Point[,] field)
+        {
+            for (int i = 1; i < Rows - 1; ++i)
+            {
+                for (int j = 1; j < Columns - 1; ++j)
+                {
+                    if (field[i, j].Ch == 'V')
+                    {
+                        field[i, j].Ch = 'A';
+                        field[i, j].Draw();
+                        Console.Write(field[i, j].Ch);
+                    }
+                }
+            }
+        }
+
+        public async Task GhostControlAsync()
+        {
+            GhostNeutrolise();
+            Task task = Task.Delay(10000);
+            await task;
+            GhostActive();
+        }
+
+        public bool Finish(Point[,] field)
+        {
+            for (int i = 1; i < Rows - 1; ++i)
+            {
+                for (int j = 1; j < Columns - 1; ++j)
+                {
+                    if (field[i, j].Ch == '.')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private Point Random(int i, int j)
+        {
+            Point point;
+            var random = new Random();
+            int value = random.Next(1, 5);
+            int num = random.Next(1, 5);
+
+            if (value == num && value == 3)
+            {
+                point = new Point(j, i, '@');
+                return point;
+            }
+            else if (value % 2 == 0)
+            {
+                point = new Point(j, i, '.');
+                return point;
+            }
+            else if (value == 3 && value < num)
+            {
+                point = new Point(j, i, 'A');
+                return point;
+            }
+            point = new Point(j, i, '.');
+            return point;
+        }
+    }
+}
---
+++ Pacman.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacmanGame
+{
+    public class Pacman
+    {
+        public enum Direction
+        {
+            LEFT,
+            RIGHT,
+            UP,
+            DOWN
+        }
+
+        public int Lives;
+        private char ch = 'o';
+        private Direction direction = Direction.DOWN;
+        public int Score = 0;
+        public int Steps = 0;
+        public bool Play = true;
+        public int X;
+        public int Y;
+
+        public Pacman(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Lives = y / 2;
+        }
+
+        public void Draw()
+        {
+            DrawPoint(ch);
+        }
+        public void Clear()
+        {
+            DrawPoint(' ');
+        }
+        private void DrawPoint(char _ch)
+        {
+            Console.SetCursorPosition(X, Y);
+            if (_ch == 'o')
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(_ch);
+        }
+
+        public void Move(string key)
+        {
+            if (key == "down")
+            {
+                direction = Direction.DOWN;
+            }
+            else if (key == "up")
+            {
+                direction = Direction.UP;
+            }
+            else if (key == "left")
+            {
+                direction = Direction.LEFT;
+            }
+            else if (key == "right")
+            {
+                direction = Direction.RIGHT;
+            }
+
+            Move();
+            CurrentResults();
+        }
+
+        public void Move()
+        {
+            ++Steps;
+            Clear();
+            GetNextPoint();
+            Draw();
+        }
+
+        public Pacman GetNextPoint()
+        {
+            switch (direction)
+            {
+                case Direction.LEFT:
+                    X -= 1;
+                    break;
+                case Direction.RIGHT:
+                    X += 1;
+                    break;
+                case Direction.UP:
+                    Y -= 1;
+                    break;
+                case Direction.DOWN:
+                    Y += 1;
+                    break;
+            }
+            var pacman = new Pacman(X, Y);
+            return pacman;
+        }
+
+        public void GetScore(char key)
+        {
+            if (key == '.') { Score += 10; }
+            else if (key == '@') { Score += 50; }
+            else if (key == 'V') { Score += 100; }
+            else if (key == 'A') { Lives -= 1; }
+        }
+
+        public void CurrentResults()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(36, 8);
+            Console.WriteLine($"Score: {Score}");
+            Console.SetCursorPosition(36, 10);
+            Console.WriteLine($"Lives: {Lives}");
+        }
+
+        public void Results()
+        {
+            Console.SetCursorPosition(36, 10);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Score: {Score}");
+            Console.SetCursorPosition(36, 12);
+            Console.WriteLine($"Steps: {Steps}");
+            Console.SetCursorPosition(36, 14);
+            Console.WriteLine($"Lives: {Lives}");
+        }
+
+        public void GameOver()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("     #####   ###   ##   ## ######");
+            Console.WriteLine("    ##      ## ##  ### ### ##");
+            Console.WriteLine("   ##      ##   ## ####### ##");
+            Console.WriteLine("   ##  ### ##   ## ####### #####");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("   ##   ## ####### ## # ## ##");
+            Console.WriteLine("    ##  ## ##   ## ##   ## ##");
+            Console.WriteLine("     ##### ##   ## ##   ## ######");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("    #####  ##   ## ###### ######");
+            Console.WriteLine("   ##   ## ##   ## ##     ##   ##");
+            Console.WriteLine("   ##   ## ##   ## ##     ##   ##");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("   ##   ## ### ### #####  ##  ###");
+            Console.WriteLine("   ##   ##  #####  ##     #####");
+            Console.WriteLine("   ##   ##   ###   ##     ## ###");
+            Console.WriteLine("    #####     #    ###### ##  ###");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void YouWin()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("   ###  ###    ######    ###   ###");
+            Console.WriteLine("   ###  ###  ###    ###  ###   ###");
+            Console.WriteLine("   ###  ###  ###    ###  ###   ###");
+            Console.WriteLine("    ######   ###    ###  ###   ###");
+            Console.WriteLine("      ##     ###    ###  ###   ###");
+            Console.WriteLine("      ##     ###    ###  ###   ###");
+            Console.WriteLine("      ##       ######     #######");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("   ###      ###  ######  ###   ###");
+            Console.WriteLine("   ###      ###   ####   ####  ###");
+            Console.WriteLine("   ###      ###   ####   ##### ###");
+            Console.WriteLine("   ###  ##  ###   ####   #########");
+            Console.WriteLine("   ############   ####   ### #####");
+            Console.WriteLine("   #####  #####   ####   ###  ####");
+            Console.WriteLine("   ###      ###  ######  ###   ###");
+        }
+    }
+}
+
